3b929a6aa725a4bad24e051d0f25863019ef9449
gwanyong_04 <43106908+YYOngYYong@users.noreply.github.com>
2020-10-08 10:24:16 +0900

    login & logout

--- src/components/login/login.jsx
+++ src/components/login/login.jsx
@@ -1,14 +1,29 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useHistory } from "react-router-dom";
 import Footer from "../footer/footer";
 import Header from "../header/header";
 import styles from "./login.module.css";
 
 const Login = ({ authService }) => {
+  const history = useHistory();
+  const goMaker = (userId) => {
+    history.push({
+      pathname: "/maker",
+      state: { id: userId },
+    });
+  };
   const onLogin = (event) => {
     authService //
       .login(event.currentTarget.textContent) //
-      .then(console.log);
+      .then((data) => goMaker(data.user.uid));
   };
+
+  //자동 로그인
+  useEffect(() => {
+    authService.onAuthChange((user) => {
+      user && goMaker(user.id);
+    });
+  });
   return (
     <section>
       <Header />
